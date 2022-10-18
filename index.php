07d54cc59751666912b652db708343fccce1ef88
andruchristo27 <100926838+andruchristo27@users.noreply.github.com>
2022-10-18 15:50:02 +0700

    page awal

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edifarm</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="menu">
+                <ul>
+                    <li><a href="" class="tbl-abu">About</a></li>
+                    <li><a href="" class="tbl-biru">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <section id="home">
+            <img src="img\bg.jpg"/>
+            
+        </section>
+</body>
+</html>

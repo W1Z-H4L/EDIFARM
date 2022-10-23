28f2b3e92b996a00bf9fea63bcc047fa0d48ac2f
andruchristo27 <100926838+andruchristo27@users.noreply.github.com>
2022-10-23 21:00:38 +0700

    sidebar ok

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,100 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<!-- Designined by CodingLab | www.youtube.com/codinglabyt -->
+<html lang="en" dir="ltr">
+  <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-</head>
+    <title> Dashboard </title>
+    <link rel="stylesheet" href="dashboard.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jaldi:wght@400;700&display=swap" rel="stylesheet">
+    <!-- Boxiocns CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
 <body>
-    
+  <div class="sidebar close">
+    <div class="logo-details">
+      <img src="img\logo1.png" alt="" class="logo1">
+      <span class="logo_name"><strong>EDI</strong>FARM</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Category</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-compass' ></i>
+          <span class="link_name">Land</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Land</a></li>
+        </ul>
+      </li>
+      <li>
+          <a href="#">
+            <i class='bx bx-book-alt' ></i>
+            <span class="link_name">Schedule</span>
+          </a>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Schedule</a></li>
+        </ul>
+      </li>
+      <li>
+          <a href="#">
+            <i class='bx bx-collection' ></i>
+            <span class="link_name">Farmer</span>
+          </a>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Farmer</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-pie-chart-alt-2' ></i>
+          <span class="link_name">Report</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Report</a></li>
+        </ul>
+      </li>
+      <li>
+          <a href="#">
+            <i class='bx bx-plug' ></i>
+            <span class="link_name">Consul</span>
+          </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Consul</a></li>
+        </ul>
+      </li>
+      <li>
+    <div class="profile-details">
+      <div class="profile-content">
+        <img src="img/profile.jpg" alt="profileImg">
+      </div>
+      <div class="name-job">
+        <div class="profile_name">Wishal</div>
+        <div class="job">Owner</div>
+      </div>
+      <i class='bx bx-log-out' ></i>
+    </div>
+  </li>
+</ul>
+  </div>
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bx-menu' ></i>
+      <span class="text">Dashboard</span>
+    </div>
+  </section>
+
+  <script src="script.js"></script>
+
 </body>
 </html>

4aa0812fe08715b0089b74c90599f3016c6f02e2
karennovitaaa <karennovita071102@gmail.com>
2022-12-05 13:53:08 +0700

    modif

--- calendar.php
+++ calendar.php
@@ -60,11 +60,28 @@
 		<?php include 'sidebar.php'; ?>
 		<div class="mobile-menu-overlay"></div>
 
+		<div class="mobile-menu-overlay"></div>
 		<div class="main-container">
-			<div class="pd-ltr-20 xs-pd-20-10">
+		<div class="pd-ltr-20 xs-pd-20-10">
 				<div class="min-height-200px">
-				<div class="title pb-20">
-						<h2 class="h3 mb-0">Jadwal</h2>
+					<div class="page-header">
+						<div class="row">
+							<div class="col-md-6 col-sm-12">
+								<div class="title">
+									<h4>Jadwal</h4>
+								</div>
+								<nav aria-label="breadcrumb" role="navigation">
+									<ol class="breadcrumb">
+										<li class="breadcrumb-item">
+											<a href="index.php">Dashboard</a>
+										</li>
+										<li class="breadcrumb-item active" aria-current="page">
+											Jadwal
+										</li>
+									</ol>
+								</nav>
+							</div>
+						</div>
 					</div>
 					<div class="pd-20 card-box mb-30">
 						<div class="calendar-wrap">

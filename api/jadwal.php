b5a4107974191191dbce07b8c35883d5e9606e9b
andruchristo27 <100926838+andruchristo27@users.noreply.github.com>
2022-12-23 13:56:10 +0700

    jadwal

--- api/jadwal.php
+++ api/jadwal.php
@@ -6,7 +6,7 @@ if (isset($_POST['id_user'])&& isset($_POST['tanggal_mulai'])) {
    $tanggal_mulai= $_POST['tanggal_mulai'];
    
 
-    $sql = "SELECT * From jadwal where id_user = $id_user and tanggal_mulai ='$tanggal_mulai' ";
+    $sql = "SELECT * From jadwal inner join sesi_tanam on jadwal.id_sesi = sesi_tanam.id_sesi inner join jenis on sesi_tanam.id_jenis=jenis.id_jenis where id_user = $id_user  and tanggal_mulai ='$tanggal_mulai' ";
     $data = mysqli_query($db, $sql);
     $rows = array();
 
